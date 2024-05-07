a00266ae63ad80a0de78b1134f86f9848e43ef83
cycle20 <46718432+cycle20@users.noreply.github.com>
2024-05-08 00:36:41 +0200

    Issue #883: refactored segfault check script

--- tests/scripts/collect_segfault_residues.php
+++ tests/scripts/collect_segfault_residues.php
@@ -6,41 +6,17 @@ use Unitmp\TmdetTest\Constants\FileSystem;
 
 const CMD = "../build/src/tmdet -i %s -x o.xml 2>&1";
 
-$lines = file('whole_para_2.log');
-$lines = preg_grep('/non-standard/', $lines);
-$lines = array_slice($lines, 0, 20);
-
+$lines = file('residues/value_error_codes.txt');
 $failedResidues = [];
-$allowedResidues = [];
+foreach ($lines as $pdbCode) {
 
-foreach ($lines as $line) {
-    if (preg_match("/InvalidArgumentException: (\S+).+?'(\S+)'/", $line, $matches)) {
-        $pdbCode = $matches[1];
-        $residue = $matches[2];
-
-        if (in_array($residue, $allowedResidues)) {
-            continue;
-        }
-
-        $cifFile = FileSystem::PDB_ZFS_DIR . '/' . substr($pdbCode, 1, 2) . "/$pdbCode.cif.gz";
-        $cmd = sprintf(CMD, $cifFile);
-        exec($cmd, $output, $resultCode);
-        $segfault = preg_grep('/core dumped/', $output) !== false;
-        if ($segfault) {
-            echo $cmd . PHP_EOL;
-            echo $residue . PHP_EOL;
-            var_dump($output);die('D');
-        }
-        if ($segfault && !array_key_exists($residue, $failedResidues)) {
-            $failedResidues[$residue] = "gdb --args $cmd";
-        } elseif (!$segfault && !in_array($residue, $allowedResidues)) {
-            $allowedResidues[] = $residue;
-        }
+    $pdbCode = trim($pdbCode);
+    $cifFile = FileSystem::PDB_ZFS_DIR . '/' . substr($pdbCode, 1, 2) . "/$pdbCode.cif.gz";
+    $cmd = sprintf(CMD, $cifFile);
+    exec($cmd, $output, $resultCode);
+    $segfault = preg_grep('/core dumped/', $output) !== false;
+    if ($segfault) {
+        $failedResidues[] = "gdb --batch -ex r -ex bt -ex q --args $cmd";
+        file_put_contents('failed_residues.ser', serialize($failedResidues));
     }
 }
-sort($failedResidues);
-sort($allowedResidues);
-file_put_contents('failed_residues.ser', serialize($failedResidues));
-file_put_contents('allowed_residues.ser', serialize($allowedResidues));
-var_dump($allowedResidues);
-
