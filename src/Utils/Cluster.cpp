f523bc2c0052e3d1e63aa0cd9ab4ffbb6d99bd56
Gabor Tusnady <tusnady.gabor@ttk.hu>
2024-08-30 12:44:04 +0200

    working on clusters

--- src/Utils/Cluster.cpp
+++ src/Utils/Cluster.cpp
@@ -3,165 +3,102 @@
 #include <iostream>
 #include <cmath>
 #include <limits>
-//#include <mlpack/methods/emst/dtb.hpp>
-//#include <mlpack/methods/emst/emst.hpp>
-//#include <mlpack/methods/dbscan.hpp>
-//#include <mlpack/core.hpp>
-//#include <mlpack.hpp>
 #include <gemmi/model.hpp>
 #include <Utils/Cluster.hpp>
 
 namespace Tmdet::Utils {
 
+    void Cluster::extractCAlphas() {
+        for (auto& chain : tmdetVO.chains) {
+            for (auto& residue : chain.residues) {
+                const gemmi::Atom* ca_atom = residue.gemmi.get_ca();
+                if (ca_atom) {
+                    cas.push_back(ca_atom->pos);
+                }
+            }
+        }
+    }
+
+    void Cluster::calculateCentroid(_cluster& cluster) {
+        cluster.centroid = gemmi::Position(0.0, 0.0, 0.0);
+        for (int point : cluster.points) {
+            cluster.centroid += cas[point]->pos;
+        }
+        cluster.centroid /= cluster.points.size();
+    }
+
+    void Cluster::calculateVariance(_cluster& cluster) {
+        cluster.variance = 0.0;
+        for (int point : cluster.points) {
+            double d = cas[point]->pos.dist(cluster.centroid);
+            cluster.variance += d * d;
+        }
+        cluster.variance = sqrt(cluster.variance);
+    }
+
+    double Cluster::calculateWardDistance(const _cluster& c1, const _cluster& c2) {
+        _cluster mergedCluster = c1;
+        mergedCluster.points.insert(mergedCluster.points.end(), c2.points.begin(), c2.points.end());
+        calculateCentroid(mergedCluster);
+        calculateVariance(mergedCluster);
+        return mergedCluster.variance - (c1.variance + c2.variance);
+    }
+
+    int Cluster::calculateChainBrakes(const _cluster& c1, const _cluster& c2) {
+        int chainBrakes = 0;
+        for (int point1 : c1.points) {
+            for (int point2: c2.points) {
+                chainBrakes += (cas[point1]->pos.dist(cas[point2]->pos) < CA_DIST);
+            }
+        }
+        return chainBrakes;
+    }
+
     void Cluster::run() {
-        
-        std::vector<gemmi::Position> ca_positions;
+        extractCAlphas();
+        int n = ca_positions.size();
+        std::vector<_cluster> clusters(n);
 
-        // Extract Cα positions
-        for (auto& chain : tmdetVO.chains) {
-            for (auto& residue : chain.residues) {
-                const gemmi::Atom* ca_atom = residue.gemmi.get_ca();
-                if (ca_atom) {
-                    ca_positions.push_back(ca_atom->pos);
+        for (int i = 0; i < n; ++i) {
+            clusters[i].points.push_back(i);
+            clusters[i].centroid = cas[i]->pos;
+            clusters[i].variance = 0.0;
+        }
+
+        while (clusters.size() > 1) {
+            double minDist = 1e30;
+            int c1Idx = -1, c2Idx = -1;
+
+            for (int i = 0; i < clusters.size(); ++i) {
+                for (int j = i + 1; j < clusters.size(); ++j) {
+                    double dist = calculateWardDistance(clusters[i], clusters[j]) 
+                        + 100 * calculateChainBrakes(clusters[i], clusters[j]);
+                    if (dist < minDist) {
+                        minDist = dist;
+                        c1Idx = i;
+                        c2Idx = j;
+                    }
                 }
             }
+
+            _cluster mergedCluster = clusters[c1Idx];
+            mergedCluster.points.insert(mergedCluster.points.end(), clusters[c2Idx].points.begin(), clusters[c2Idx].points.end());
+            calculateCentroid(mergedCluster);
+            calculateVariance(mergedCluster);
+
+            clusters.erase(clusters.begin() + c2Idx);
+            clusters.erase(clusters.begin() + c1Idx);
+            clusters.push_back(mergedCluster);
+
+            std::cout << "Merged clusters " << cas[c1Idx].serial << " and " << cas[c2Idx].serial << " with distance " << minDist << std::endl;
         }
+
+        std::cout << "Final cluster: ";
+        for (int point : clusters[0].points) {
+            std::cout << point << " ";
+        }
+        std::cout << std::endl;
+    }
+
     
-        // Create the distance matrix
-        size_t n = ca_positions.size();
-        arma::mat distance_matrix(n, n);
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = i + 1; j < n; ++j) {
-                double distance = ca_positions[i].dist(ca_positions[j]);
-                distance_matrix(i, j) = distance_matrix(j, i) = distance;
-            }
-        }
-
-        // Run hierarchical agglomerative clustering
-        //arma::Row<size_t> assignments;
-        //mlpack::tree::DBSCAN<> hdbscan(distance_matrix, assignments, 2);
-
-        // Adjust distance matrix for DBSCAN (DBSCAN requires a set of points, not a distance matrix)
-        arma::mat ca_positions_mat(3, n);
-        for (size_t i = 0; i < n; ++i) {
-            ca_positions_mat(0, i) = ca_positions[i].x;
-            ca_positions_mat(1, i) = ca_positions[i].y;
-            ca_positions_mat(2, i) = ca_positions[i].z;
-        }
-
-        // Set DBSCAN parameters
-        double epsilon = 5.0; // Adjust as needed
-        size_t minPoints = 10; // Minimum number of points in a cluster
-
-        /*
-        // Run DBSCAN
-        arma::Row<size_t> assignments;
-        assignments.clear();
-        mlpack::dbscan::DBSCAN<> dbscan(epsilon, minPoints);
-        dbscan.Cluster(ca_positions_mat, assignments);
-
-        //Write back the clustering results to residues any 
-        size_t i = 0;
-        for (auto& chain : tmdetVO.chains) {
-            for (auto& residue : chain.residues) {
-                const gemmi::Atom* ca_atom = residue.gemmi.get_ca();
-                if (ca_atom) {
-                     residue.temp.insert({"cluster",std::any_cast<size_t>(assignments[i++])});
-                }
-            }
-        }*/
-
-
-        
-
-using namespace std;
-
-struct Cluster {
-    vector<int> points;
-    double centroid;
-};
-
-double calculateCentroid(const vector<double>& data, const Cluster& cluster) {
-    double sum = 0.0;
-    for (int point : cluster.points) {
-        sum += data[point];
-    }
-    return sum / cluster.points.size();
-}
-
-double calculateWardDistance(const Cluster& c1, const Cluster& c2, const vector<double>& data) {
-    double centroid1 = calculateCentroid(data, c1);
-    double centroid2 = calculateCentroid(data, c2);
-
-    double withinClusterVariance1 = 0.0;
-    for (int point : c1.points) {
-        withinClusterVariance1 += pow(data[point] - centroid1, 2);
-    }
-
-    double withinClusterVariance2 = 0.0;
-    for (int point : c2.points) {
-        withinClusterVariance2 += pow(data[point] - centroid2, 2);
-    }
-
-    Cluster mergedCluster = c1;
-    mergedCluster.points.insert(mergedCluster.points.end(), c2.points.begin(), c2.points.end());
-    double mergedCentroid = calculateCentroid(data, mergedCluster);
-
-    double withinClusterVarianceMerged = 0.0;
-    for (int point : mergedCluster.points) {
-        withinClusterVarianceMerged += pow(data[point] - mergedCentroid, 2);
-    }
-
-    return withinClusterVarianceMerged - (withinClusterVariance1 + withinClusterVariance2);
-}
-
-void hierarchicalClustering(const vector<double>& data) {
-    int n = data.size();
-    vector<Cluster> clusters(n);
-
-    for (int i = 0; i < n; ++i) {
-        clusters[i].points.push_back(i);
-        clusters[i].centroid = data[i];
-    }
-
-    while (clusters.size() > 1) {
-        double minDist = numeric_limits<double>::max();
-        int c1Idx = -1, c2Idx = -1;
-
-        for (int i = 0; i < clusters.size(); ++i) {
-            for (int j = i + 1; j < clusters.size(); ++j) {
-                double dist = calculateWardDistance(clusters[i], clusters[j], data);
-                if (dist < minDist) {
-                    minDist = dist;
-                    c1Idx = i;
-                    c2Idx = j;
-                }
-            }
-        }
-
-        Cluster mergedCluster = clusters[c1Idx];
-        mergedCluster.points.insert(mergedCluster.points.end(), clusters[c2Idx].points.begin(), clusters[c2Idx].points.end());
-        mergedCluster.centroid = calculateCentroid(data, mergedCluster);
-
-        clusters.erase(clusters.begin() + c2Idx);
-        clusters.erase(clusters.begin() + c1Idx);
-        clusters.push_back(mergedCluster);
-
-        cout << "Merged clusters " << c1Idx << " and " << c2Idx << " with distance " << minDist << endl;
-    }
-
-    cout << "Final cluster: ";
-    for (int point : clusters[0].points) {
-        cout << point << " ";
-    }
-    cout << endl;
-}
-
-int main() {
-    vector<double> data = {1.0, 2.0, 5.0, 8.0, 10.0};
-    hierarchicalClustering(data);
-    return 0;
-}
-
-    }
 }
--- src/Utils/Cluster.hpp
+++ src/Utils/Cluster.hpp
@@ -1,14 +1,29 @@
 #ifndef __TMDET_UTILS_CLUSTER__
 #define __TMDET_UTILS_CLUSTER__
 
+#include <gemmi/model.hpp>
 #include <ValueObjects/TmdetStruct.hpp>
 
+#define CA_DIST 4.0
+
 namespace Tmdet::Utils {
+
+    struct _cluster {
+        std::vector<int> points;
+        gemmi::Position centroid;
+        double variance;
+    };
 
     class Cluster {
         private:
             Tmdet::ValueObjects::TmdetStruct& tmdetVO;
-
+            std::vector<gemmi::Atom *> cas;
+            
+            void extractCAlphas();
+            void calculateCentroid(_cluster& cluster);
+            void calculateVariance(_cluster& cluster);
+            double calculateWardDistance(const _cluster& c1, const _cluster& c2);
+            int calculateChainBrakes(const _cluster& c1, const _cluster& c2);
             
         public:
             Cluster(Tmdet::ValueObjects::TmdetStruct& _tmdetVO) : tmdetVO(_tmdetVO) {} ;
