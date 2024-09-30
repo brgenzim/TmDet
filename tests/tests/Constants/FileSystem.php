7fa6593dd316148623cff4549a6e6bda1a8eafc8
cycle20 <46718432+cycle20@users.noreply.github.com>
2024-09-30 14:26:00 +0200

    Issue #943: PdbtmDifferencesTest: includes only TmAlpha; tests only 'H' regions

--- tests/tests/Constants/FileSystem.php
+++ tests/tests/Constants/FileSystem.php
@@ -6,4 +6,5 @@ class FileSystem {
     const PDB_ZFS_DIR = '/zfs/databases/UniTmp/PDB/data/structures/divided/mmCIF';
     const PDB_ENT_ZFS_DIR = '/zfs/databases/UniTmp/PDB/data/structures/divided/pdb';
     const PDBTM30_DATA_DIR = '/home/tusi/works/pdbtm_3.0/data';
+    const PDBTM_ALL_XML_FILE = '/home/tusi/works/pdbtm_3.0/data/pdbtm_all.xml';
 }
--- tests/tests/PdbtmDifferencesTest.php
+++ tests/tests/PdbtmDifferencesTest.php
@@ -8,26 +8,73 @@ use PHPUnit\Framework\TestCase;
 use Unitmp\TmdetTest\Constants\FileSystem;
 use Unitmp\TmdetTest\Services\Tmdet30Runner;
 
+use function PHPUnit\Framework\assertEquals;
+
 class PdbtmDifferencesTest extends TestCase {
 
     const PDB_ENT_DIR = FileSystem::PDB_ENT_ZFS_DIR;
+    const PDBTM_ALL_XML_FILE = FileSystem::PDBTM_ALL_XML_FILE;
     const MAX_ALLOWED_LEVENSHTEIN_DISTANCE_IN_PERCENT = 40;
+    const MAX_ALLOWED_RESIDUE_DIFFERENCE = 4;
 
     #[Group('tmdet')]
     #[DataProvider('getEntPathsOfTmps')]
     public function test_pdbtm_manual_updates(string $entPath) {
         // Arrange
         $tmdetRunner = Tmdet30Runner::createRunner($entPath);
+        if ($tmdetRunner->oldData['tmType'] !== 'Tm_Alpha') {
+            printf("Skipping test (not Tm_Alpha): %s\n", $tmdetRunner->oldTmdetFile);
+            $this->markTestSkipped('Not Tm_Alpha ' . $entPath);
+        }
+
         // Act
         $tmdetSuccess = $tmdetRunner->exec();
         // Assert
         $this->assertTrue($tmdetSuccess);
-        $this->assertRegionArraysEqual($tmdetRunner->oldData, $tmdetRunner->newData);
+        $this->assertTmpValueEquals($tmdetRunner);
+        //$this->assertRegionArraysEqual($tmdetRunner->oldData, $tmdetRunner->newData);
     }
 
     //
     // Util functions
     //
+
+    /**
+     * Assert TM protein values are equal or their difference is small enough.
+     */
+    public function assertTmpValueEquals(Tmdet30Runner $runner) {
+        $oldValue = $runner->oldData['isTmp'];
+        $newValue = $runner->newData['isTmp'];
+        $this->assertEquals($oldValue, $newValue, '"isTmp" values differ');
+
+        $oldValue = $runner->oldData['chains'];
+        $newValue = $runner->newData['chains'];
+        $this->assertEquals(array_keys($oldValue), array_keys($newValue),
+            'Different chain lists in TMDET results');
+
+        $helixFilter = function ($regions) {
+            return array_filter($regions, function ($value) {
+                return $value === 'H';
+            } );
+        };
+        foreach ($oldValue as $chain => $chainData) {
+            assertEquals($chainData['num_tm'], $newValue[$chain]['num_tm'],
+                'Number of TM regions differs');
+            $newRegions = $helixFilter($newValue[$chain]['regions']);
+            foreach ($helixFilter($chainData['regions']) as $index => $oldRegion) {
+                $newRegion = $newRegions[$index];
+                if ($oldRegion['type'] == 'H') {
+                    $this->assertEquals($oldRegion['type'], $newRegion['type'],
+                        'Region type of chain "' . $chain . '"[new seq_beg: ' . $newRegion['seq_beg'] . '] differs');
+                }
+                $this->assertEqualsWithDelta($oldRegion['seq_beg'], $newRegion['seq_beg'], static::MAX_ALLOWED_RESIDUE_DIFFERENCE,
+                    'Region start in chain "' . $chain . '"[new seq_beg: ' . $newRegion['seq_beg'] . '] differs');
+                $this->assertEqualsWithDelta($oldRegion['seq_end'], $newRegion['seq_end'], static::MAX_ALLOWED_RESIDUE_DIFFERENCE,
+                    'Region end in chain "' . $chain . '"[new seq_end: ' . $newRegion['seq_end'] . '] differs');
+            }
+        }
+    }
+
     public function assertRegionArraysEqual(array $expected, array $actual) {
         $this->assertEquals(array_keys($expected), array_keys($actual));
         $ok = true;
@@ -66,15 +113,18 @@ class PdbtmDifferencesTest extends TestCase {
     }
 
     public static function getEntPathsOfTmps(): array {
+        $codes = Tmdet30Runner::getPdbtmCodes(static::PDBTM_ALL_XML_FILE);
+
         $paths = [];
-        foreach (explode("\n", trim(file_get_contents('../data/tm-codes.txt'))) as $code) {
+        foreach ($codes as $code => $item) {
             $subDir = '/' . $code[1] . $code[2] . '/';
             $fileName = "pdb$code.ent.gz";
             $fullPath = static::PDB_ENT_DIR . $subDir . $fileName;
-            $paths[ $fileName ] = [ $fullPath ];
+            $paths[ $fileName ] = [ $fullPath ]; //  , $item['isTmp']
         }
+        ksort($paths);
+        // return array_slice($paths, 0, 100);
         return $paths;
-        // return array_slice($paths, 0, 5);
     }
 
 }
