ca3c99f0dda5d291ffd2721a2f5c03072a0a4b99
cycle20 <46718432+cycle20@users.noreply.github.com>
2024-10-04 16:34:00 +0200

    Issue #943: refactored comparator, tmdiff can writes categories into Excel spreadsheet

--- tests/tests/Services/PdbtmComparator.php
+++ tests/tests/Services/PdbtmComparator.php
@@ -10,12 +10,13 @@ class PdbtmComparator {
 
         $details = [];
         $messages = [];
+        $categories = [];
 
-        $oldValue = $runner->oldData['isTmp'];
-        $newValue = $runner->newData['isTmp'];
-        if ($oldValue != $newValue) {
-            static::appendMessage(true, $messages, '"isTmp" values differ');
-            return [ 'messages' => $messages ];
+        $oldTmp = $runner->oldData['isTmp'];
+        $newTmp = $runner->newData['isTmp'];
+        if ($oldTmp != $newTmp) {
+            $category = 'TMP attributes differ';
+            return [ 'categories' => [ $category ] ];
         }
 
         $oldDeleteChains = $runner->oldData['deletedChains'];
@@ -23,26 +24,45 @@ class PdbtmComparator {
         $oldApplyToChains = $runner->oldData['addedChains'];
         $newApplyToChains = $runner->newData['addedChains'];
 
-        $errorEncountered = empty($newDeleteChains) && !empty($oldDeleteChains);
-        static::appendMessage($errorEncountered, $messages, 'Old TMDET XML has deleted chains, but new XML hasn\'t contain any');
-        $errorEncountered = !empty($newDeleteChains) && empty($oldDeleteChains);
-        static::appendMessage($errorEncountered, $messages, 'New TMDET XML has deleted chains, but old XML hasn\'t contain any');
-        static::appendMessage($oldDeleteChains != $newDeleteChains, $messages, 'Deleted chain list differs');
+        static::updateDetails(
+            condition: empty($newDeleteChains) && !empty($oldDeleteChains),
+            categories: $categories, category: 'Old TMDET XML has deleted chains, but new XML hasn\'t contain any'
+        );
 
-        $errorEncountered = empty($newApplyToChains) && !empty($oldApplyToChains);
-        static::appendMessage($errorEncountered, $messages, 'Old TMDET XML has added chains, but new XML hasn\'t contain any');
-        $errorEncountered = !empty($newApplyToChains) && empty($oldApplyToChains);
-        static::appendMessage($errorEncountered, $messages, 'New TMDET XML has added chains, but old XML hasn\'t contain any');
-        static::appendMessage($oldApplyToChains != $newApplyToChains, $messages, 'Added chain list differs');
+        static::updateDetails(
+            condition: !empty($newDeleteChains) && empty($oldDeleteChains),
+            categories: $categories, category: 'New TMDET XML has deleted chains, but old XML hasn\'t contain any'
+        );
+
+        static::updateDetails(
+            condition: $oldDeleteChains != $newDeleteChains,
+            categories: $categories, category: 'Deleted chain list differs'
+        );
+
+        static::updateDetails(
+            condition: empty($newApplyToChains) && !empty($oldApplyToChains),
+            categories: $categories, category: 'Old TMDET XML has added chains, but new XML hasn\'t contain any'
+        );
+        static::updateDetails(
+            condition: !empty($newApplyToChains) && empty($oldApplyToChains),
+            categories: $categories, category: 'New TMDET XML has added chains, but old XML hasn\'t contain any'
+        );
+
+        static::updateDetails(
+            condition: $oldApplyToChains != $newApplyToChains,
+            categories: $categories, category: 'Added chain list differs'
+        );
 
         $oldChains = $runner->oldData['chains'];
         $newChains = $runner->newData['chains'];
         $details['oldData'] = [
+            'isTmp' => $oldTmp,
             'chains' => array_keys($oldChains),
             'addedChains' => $oldApplyToChains,
             'deletedChains' => $oldDeleteChains
         ];
         $details['newData'] = [
+            'isTmp' => $newTmp,
             'chains' => array_keys($newChains),
             'addedChains' => $newApplyToChains,
             'deletedChains' => $newDeleteChains
@@ -51,8 +71,10 @@ class PdbtmComparator {
         static::synchronizeBioMatrixDeletes($oldChains, $newChains, $oldDeleteChains, $newDeleteChains);
         static::synchronizeBioMatrixApply($oldChains, $oldApplyToChains);
 
-        $errorEncountered = !empty(array_diff_key($oldChains, $newChains)) || !empty(array_diff_key($newChains, $oldChains));
-        static::appendMessage($errorEncountered, $messages, 'Different chain lists in TMDET results');
+        static::updateDetails(
+            condition: !empty(array_diff_key($oldChains, $newChains)) || !empty(array_diff_key($newChains, $oldChains)),
+            categories: $categories, category: 'Different chain lists in TMDET results'
+        );
 
         $helixFilter = function ($regions) {
             return array_filter($regions, function ($value) {
@@ -60,35 +82,69 @@ class PdbtmComparator {
             } );
         };
         foreach ($oldChains as $chain => $chainData) {
-            static::appendMessage($chainData['num_tm'] != $newChains[$chain]['num_tm'], $messages,
-                'Number of TM regions differs - chain: ' . $chain);
+            $category = 'Number of TM regions differs';
+            $message = $category . ' - chain: ' . $chain;
+            static::updateDetails(
+                condition: $chainData['num_tm'] != $newChains[$chain]['num_tm'],
+                messages: $messages, categories: $categories,
+                message: $message, category: $category
+            );
             $newRegions = $helixFilter($newChains[$chain]['regions']);
             foreach ($helixFilter($chainData['regions']) as $index => $oldRegion) {
                 $newRegion = $newRegions[$index];
                 if ($oldRegion['type'] == 'H') {
-                    static::appendMessage($oldRegion['type'] != $newRegion['type'], $messages,
-                        'Region type of chain "' . $chain . '"[new seq_beg: ' . $newRegion['seq_beg'] . '] differs');
+                    static::updateDetails(
+                        condition: $oldRegion['type'] != $newRegion['type'],
+                        messages: $messages, categories: $categories,
+                        message: 'Region type of chain "' . $chain . '"[new seq_beg: ' . $newRegion['seq_beg'] . '] differs',
+                        category: 'Region type of chain differs'
+                    );
                 }
 
                 $actualValue = $newRegion['seq_beg'];
-                $errorEncountered = !static::equalsWithDelta($oldRegion['seq_beg'], $actualValue, static::MAX_ALLOWED_RESIDUE_DIFFERENCE);
-                static::appendMessage($errorEncountered, $messages,
-                    'Region start in chain "' . $chain . '"[new seq_beg: ' . $actualValue . '] differs');
+                static::updateDetails(
+                    condition: !static::equalsWithDelta($oldRegion['seq_beg'], $actualValue, static::MAX_ALLOWED_RESIDUE_DIFFERENCE),
+                    messages: $messages, categories: $categories,
+                    message: 'Region start in chain "' . $chain . '"[new seq_beg: ' . $actualValue . '] differs',
+                    category: 'Region start/end in chain differs'
+                );
 
                 $actualValue = $newRegion['seq_end'];
-                $errorEncountered = !static::equalsWithDelta($oldRegion['seq_end'], $actualValue, static::MAX_ALLOWED_RESIDUE_DIFFERENCE);
-                static::appendMessage($errorEncountered, $messages,
-                    'Region end in chain "' . $chain . '"[new seq_end: ' . $actualValue . '] differs');
+                static::updateDetails(
+                    condition: !static::equalsWithDelta($oldRegion['seq_end'], $actualValue, static::MAX_ALLOWED_RESIDUE_DIFFERENCE),
+                    messages: $messages, categories: $categories,
+                    message: 'Region end in chain "' . $chain . '"[new seq_end: ' . $actualValue . '] differs',
+                    category: 'Region start/end in chain differs'
+                );
             }
         }
 
-        $details['messages'] = $messages;
+        $details['categories'] = $categories;
+        if (!empty($messages)) {
+            $details['messages'] = $messages;
+        }
         return $details;
+    }
+
+    protected static function updateDetails(bool $condition, array &$categories, string $category,
+        array|false &$messages = false, string|false $message = false): void {
+
+        static::addToCategory($condition, $categories, $category);
+        if (!$message || $messages === false) {
+            return;
+        }
+        static::appendMessage($condition, $messages, $message);
     }
 
     protected static function appendMessage(bool $condition, array &$messages, string $message): void {
         if ($condition) {
             $messages[] = $message;
+        }
+    }
+
+    protected static function addToCategory(bool $condition, array &$categories, string $category): void {
+        if ($condition && array_search($category, $categories) === false) {
+            $categories[] = $category;
         }
     }
 
