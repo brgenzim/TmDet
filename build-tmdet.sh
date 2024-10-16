18d93748bbec09e75639a8acebfe15e06bba9225
cycle20 <46718432+cycle20@users.noreply.github.com>
2024-10-16 13:47:10 +0200

    Mount points added to docker container start

--- build-tmdet.sh
+++ build-tmdet.sh
@@ -1,4 +1,10 @@
-docker run --user $(id -u):$(id -g) \
-    -it --mount type=bind,source=$(pwd),target=/usr/local/src/tmdet \
+docker run --rm --user $(id -u):$(id -g) -it \
+    --mount type=bind,source=$(pwd),target=/usr/local/src/tmdet \
+    --mount type=bind,source=/zfs,target=/zfs \
+    --mount type=bind,source=/home/tusi,target=/home/tusi \
+    --mount type=bind,source=/home/B,target=/home/B \
+    --mount type=bind,source=/home/data,target=/home/data \
+    --mount type=bind,source=/data,target=/data \
     tmdet:4.0.0 \
-    /bin/bash -c 'cmake -B build && make -C build tmdet; /bin/bash'
+    /bin/bash -c "$1"
+    #/bin/bash -c 'cmake -B build && make -C build tmdet; /bin/bash'
