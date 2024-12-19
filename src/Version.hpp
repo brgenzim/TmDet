bd48ccecd4ea07cf75a76d3c14b8f95e4daf65f1
tusi <tusi@enzim.hu>
2024-12-19 15:27:26 +0100

    version 4.0.0

--- src/Version.hpp
+++ src/Version.hpp
@@ -13,10 +13,6 @@
 #define TMDET_VERSION_MINOR 0
 #define TMDET_VERSION_PATCH 0
 
-#define TMDET_VERSION_DEVELOP_MAJOR 0
-#define TMDET_VERSION_DEVELOP_MINOR 8
-#define TMDET_VERSION_DEVELOP_PATCH 0
-
 /**
  * @namespace Tmdet
  */
@@ -28,8 +24,7 @@ namespace Tmdet {
     * @return std::string 
     */
     static std::string version() {
-        return std::format("{}.{}.{}-{}.{}.{}",
-                    TMDET_VERSION_MAJOR, TMDET_VERSION_MINOR, TMDET_VERSION_PATCH,
-                    TMDET_VERSION_DEVELOP_MAJOR, TMDET_VERSION_DEVELOP_MINOR, TMDET_VERSION_DEVELOP_PATCH);
+        return std::format("{}.{}.{}",
+                    TMDET_VERSION_MAJOR, TMDET_VERSION_MINOR, TMDET_VERSION_PATCH);
     }
 }
