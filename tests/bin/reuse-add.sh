28144debf9b8e490e69565c4c32d65d03dc324be
cycle20 <46718432+cycle20@users.noreply.github.com>
2025-03-31 10:48:28 +0200

    Removed test folder

--- tests/bin/reuse-add.sh
+++
@@ -1,8 +0,0 @@
-ARGS="--copyright-prefix spdx-symbol --license 'CC-BY-NC-4.0' --year 2003"
-ARGS="$ARGS --copyright 'HUN-REN Research Center of Natural Sciences'"
-ARGS="$ARGS --copyright 'Gabor E. Tusnady <tusnady.gabor@ttk.hu'"
-ARGS="$ARGS --contributor 'Csongor Gerdan <gerdan.csongor@ttk.hu'"
-
-FILES="src/cli/tmdet.cpp src/CMakeLists.txt"
-
-eval pipx run reuse annotate $ARGS $FILES
