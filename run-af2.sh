4b5e456a4d1b22d58e3f210654f68267d1ecda02
cycle20 <46718432+cycle20@users.noreply.github.com>
2025-03-31 10:53:38 +0200

    Removed run* scripts and unused REUSE.toml

--- run-af2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-AF_ROOT="/bigdisk/users/tusi/pdbtm/data/AlphaFold/new/test/afdb"
-TMDET_ROOT="/bigdisk/users/tusi/pdbtm/data/AlphaFold/new/test/tmdet"
-
-echo "./bin/tmdet -r -pi $AF_ROOT/AF-$1-F1-model_v4.cif -po $TMDET_ROOT/$1.cif.gz -xo $TMDET_ROOT/$1.xml -fr -minht 13"
-./bin/tmdet -r -pi $AF_ROOT/AF-$1-F1-model_v4.cif -po $TMDET_ROOT/$1.cif.gz -xo $TMDET_ROOT/$1.xml -fr -minht 13
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./bin/tmdet -r -npf -c $*
